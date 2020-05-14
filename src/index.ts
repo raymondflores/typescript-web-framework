a29853b22f15e69701001b0b3683162fa6a49f5e
Raymond Flores <raymondfflores@gmail.com>
2020-05-14 02:30:44 -0500

    adding event register method to user class

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Raymond', age: 27 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+  console.log('hi');
+});
+
+console.log(user);
