7198695dee0d9bdecbc80291c30e2de34e634641
Raymond Flores <raymondfflores@gmail.com>
2020-05-14 18:46:41 -0500

    added json-server and fetch method

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Raymond', age: 27 });
+const user = new User({ id: 1 });
+user.fetch();
 
-user.on('change', () => {
-  console.log('change 1');
-});
-
-user.on('change', () => {
-  console.log('change 2');
-});
-
-user.on('click', () => {
-  console.log('click');
-});
-
-user.trigger('change');
-user.trigger('click');
+console.log(user);
