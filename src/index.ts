4df9720f475ae6c7ad0ca5e65dd285c654e6c816
Raymond Flores <raymondfflores@gmail.com>
2020-05-14 02:34:37 -0500

    add method to trigger registered events for user class

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,16 @@ import { User } from './models/User';
 const user = new User({ name: 'Raymond', age: 27 });
 
 user.on('change', () => {
-  console.log('hi');
+  console.log('change 1');
 });
 
-console.log(user);
+user.on('change', () => {
+  console.log('change 2');
+});
+
+user.on('click', () => {
+  console.log('click');
+});
+
+user.trigger('change');
+user.trigger('click');
