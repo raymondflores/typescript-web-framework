2bb921a34b292838e633eecdfc7c623598f3e00b
Raymond Flores <raymondfflores@gmail.com>
2020-05-16 02:02:56 -0500

    finishing up views and project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'raymond', age: 27 });
+const root = document.getElementById('root');
 
-const userForm = new UserForm(document.getElementById('root'), user);
-userForm.render();
+if (root) {
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  console.log(userEdit);
+} else {
+  throw new Error('Root element not found.');
+}
