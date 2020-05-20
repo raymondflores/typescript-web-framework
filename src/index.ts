cadcabbe70081b99fd0657af1d221c0cd78b74c7
Raymond Flores <raymondfflores@gmail.com>
2020-05-20 16:24:35 -0500

    finished framework

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+// import { UserEdit } from './views/UserEdit';
+// import { User } from './models/User';
 
-const user = User.buildUser({ name: 'raymond', age: 27 });
-const root = document.getElementById('root');
+// const user = User.buildUser({ name: 'raymond', age: 27 });
+// const root = document.getElementById('root');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('Root element not found.');
-}
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
+//   userEdit.render();
+//   console.log(userEdit);
+// } else {
+//   throw new Error('Root element not found.');
+// }
+
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
+
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
